b78ac602e667e0bea41d895546fb1cae52d3f8e2
Shannon Trudeau <smt9020@rit.edu>
2014-03-05 22:04:13 -0500

    adding in androguard

--- stowawayScript.sh
+++ stowawayScript.sh
@@ -5,18 +5,32 @@ clear
 echo "The script starts now"
 echo
 
-pushd ../tools/stowaway/stowaway/Stowaway-1.2.4
+pushd ./tools/stowaway/Stowaway-1.2.4
 
-FILES=../../../../ProjectKrutz/downloads/*
+FILES=../../../ProjectKrutz/tools/testAndroidApps/*
 for f in $FILES
 do
 	APK="apkOutput/"
 	OUTPUT="_output"
-	OUTPUT_FOLDER=$APK${f#../../../../ProjectKrutz/downloads/}$OUTPUT
+	OUTPUT_FOLDER=$APK${f#../../../ProjectKrutz/tools/testAndroidApps/}$OUTPUT
 	echo $f
 	echo $OUTPUT_FOLDER
 
 	bash stowaway.sh $f ../$OUTPUT_FOLDER &>output.txt
+done
+
+popd
+
+echo "Stowaway Complete"
+echo "Starting Androguard"
+
+pushd ./tools/Androguard
+
+FILES=../../../ProjectKrutz/tools/testAndroidApps/*
+for f in $FILES
+do
+	echo $f
+	bash ./androrisk.py -m -i ../testAndroidApps/$f
 done
 
 popd
