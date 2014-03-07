0137b2e0bc1a7b42f8a869cfd334642255be91b8
cklimkowsky <cklimkowsky@gmail.com>
2014-03-07 16:05:34 -0500

    Working on scraping the Google Play store... Need to work on tokens for POST request to evozi.com

---
+++ scraper/scraper/spiders/googlePlaySpider.py
@@ -0,0 +1,117 @@
+from scrapy import log
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import Selector
+from scrapy.http import Request
+from scraper.items import ApkItem
+from play import parse_google
+import re
+import string
+import requests
+
+class GooglePlaySpider(CrawlSpider):
+	name = 'googleplay'
+	allowed_domains = ['play.google.com', 'apps.evozi.com', 'api.evozi.com', 'storage.evozi.com']
+	start_urls = [
+		'https://play.google.com/store/apps'
+	]
+	rules = (
+		# Rule(SgmlLinkExtractor(allow=('/store/apps$', )), callback='parse_category_group', follow=True),
+		Rule(SgmlLinkExtractor(allow=('/store/apps/category/.*', )), callback='parse_category', follow=True),
+		# Rule(SgmlLinkExtractor(allow=('/store/search\?.*', )), callback='parse_search', follow=True),
+	)
+
+	def parse_category_group(self, response):
+		sel = Selector(response)
+		category_groups = sel.xpath('//div[@class="padded-content3 app-home-nav"]')
+
+		for category_group in category_groups:
+
+			category_group_name = category_group.xpath('h2/a/text()').extract()
+
+			categories = category_group.xpath('ul/li')
+			for category in categories:
+				category_name = category.xpath('a/text()').extract()
+				category_url = category.xpath('a/@href').extract()[0]
+
+		chars = string.ascii_uppercase + string.digits
+		for x in chars:
+			yield Request('https://play.google.com/store/search?q=' + x + '&c=apps', callback=self.parse_search)
+
+		for x in chars:
+			for y in chars:
+				yield Request('https://play.google.com/store/search?q=' + x + y + '&c=apps', callback=self.parse_search)
+
+		for x in chars:
+			for y in chars:
+				for z in chars:
+					yield Request('https://play.google.com/store/search?q=' + x + y + z + '&c=apps', callback=self.parse_search)        
+
+		return
+
+	def parse_category(self, response):
+		base_path = response.url.split('?')[0]   
+
+		if '/collection/' in response.url:
+			sel = Selector(response)
+			apps = sel.xpath('//a[@class="title"]')
+			has_app = False
+
+			for app in apps:
+				has_app = True
+				app_name = app.xpath('text()').extract()
+				app_url = app.xpath('@href').extract()
+				yield Request('https://play.google.com' + app_url[0], callback=self.parse_app)
+
+			if has_app:
+				m = re.match(r'(.*)\?start=(\d+)&num=24', response.url)
+				if m is None:
+					start_number = 24                  
+				else:
+					start_number = int(m.group(2)) + 24
+				# yield Request(base_path + '?start=' + str(start_number) + '&num=24', callback=self.parse_category)
+
+		return
+
+	def parse_search(self, response):
+		m = re.match(r'(.*)&start=(\d+)&num=24', response.url)
+		if m is None:
+			base_path = response.url
+			start_number = 24                  
+		else:
+			start_number = int(m.group(2)) + 24
+			base_path = m.group(1)
+
+		sel = Selector(response)
+		apps = sel.xpath('//a[contains(@href,"/store/apps/details")]')
+		has_app = False
+
+		for app in apps:
+			has_app = True
+			app_url = app.xpath('@href').extract()
+			yield Request('https://play.google.com' + app_url[0], callback=self.parse_app)
+
+		if has_app:
+			yield Request(base_path + '&start=' + str(start_number) + '&num=24', callback=self.parse_search)
+
+		return
+
+	def parse_app(self, response):
+		sel = Selector(response)
+		price = sel.xpath('//meta[@itemprop="price"]/@content').extract()[0]
+
+		try:
+			if int(price) == 0:
+				package_name = response.url[response.url.find('id=') + 3:]
+				url = 'http://api.evozi.com/apk-downloader/download'
+        		data = {
+            		'packagename': package_name, 
+            		'fccfeadfb': 'Hs6RVv7hBbUgLkDqUz5adQ', 
+            		't': '1394221463', 
+            		'fetch': 'false'
+        		}
+        		post_response = requests.post(url, data=data)
+        		json = post_response.json()	# Invalid or Expired Token, Please refresh the page and try again
+				# yield Request(response.url, meta={'url': response.url, 'file_urls': [json['url']]}, callback=parse_google)
+		except ValueError:
+			log.msg('Not a free app: ' + response.url, log.INFO)
